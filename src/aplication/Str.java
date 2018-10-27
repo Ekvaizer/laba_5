43b38a861a5f336f128b0aaf2e1d75e3a4ec1017
Alexander Kulinich <380680188847@yandex.ua>
2018-10-27 11:15:16 +0300

    Update Str.java

--- src/aplication/Str.java
+++ src/aplication/Str.java
@@ -7,21 +7,21 @@ public class Str {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
-		System.out.println("Введите строку");
+		System.out.println("Р’РІРµРґРёС‚Рµ СЃС‚СЂРѕРєСѓ");
 		String string =scn.nextLine();
-		System.out.println("Входная буква в нижнем регистре  ");
- String a=scn.nextLine();
- System.out.println("Входная буква в верхнем регистре  ");
- String aa=scn.nextLine();
- System.out.println("Выходная буква в нижнем регистре  ");
- String b=scn.nextLine();
- System.out.println("Выходная буква в верхнем регистре  ");
- String bb=scn.nextLine();
- String q=(string.replace(a, b));
+		System.out.println("Р’С…РѕРґРЅР°СЏ Р±СѓРєРІР° РІ РЅРёР¶РЅРµРј СЂРµРіРёСЃС‚СЂРµ  ");
+		String a=scn.nextLine();
+		System.out.println("Р’С…РѕРґРЅР°СЏ Р±СѓРєРІР° РІ РІРµСЂС…РЅРµРј СЂРµРіРёСЃС‚СЂРµ  ");
+		String aa=scn.nextLine();
+		System.out.println("Р’С‹С…РѕРґРЅР°СЏ Р±СѓРєРІР° РІ РЅРёР¶РЅРµРј СЂРµРіРёСЃС‚СЂРµ  ");
+		String b=scn.nextLine();
+		System.out.println("Р’С‹С…РѕРґРЅР°СЏ Р±СѓРєРІР° РІ РІРµСЂС…РЅРµРј СЂРµРіРёСЃС‚СЂРµ  ");
+		String bb=scn.nextLine();
+		String q=(string.replace(a, b));
 
- String s=(q.replace(aa, bb));
- System.out.println(s);
- scn.close();
+		String s=(q.replace(aa, bb));
+		System.out.println(s);
+		scn.close();
 	}
 
 }
