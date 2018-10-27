88fb660aa221d5ff90e11cfeedb7af8482d24af2
Alexander Kulinich <380680188847@yandex.ua>
2018-10-27 11:14:07 +0300

    Add files via upload

---
+++ src/aplication/Str.java
@@ -0,0 +1,27 @@
+package aplication;
+
+import java.util.Scanner;
+
+public class Str {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Введите строку");
+		String string =scn.nextLine();
+		System.out.println("Входная буква в нижнем регистре  ");
+ String a=scn.nextLine();
+ System.out.println("Входная буква в верхнем регистре  ");
+ String aa=scn.nextLine();
+ System.out.println("Выходная буква в нижнем регистре  ");
+ String b=scn.nextLine();
+ System.out.println("Выходная буква в верхнем регистре  ");
+ String bb=scn.nextLine();
+ String q=(string.replace(a, b));
+
+ String s=(q.replace(aa, bb));
+ System.out.println(s);
+ scn.close();
+	}
+
+}
---
+++ src/aplication/str_v2.java
@@ -0,0 +1,13 @@
+package aplication;
+
+public class str_v2 {
+
+	public static void main(String[] args) {
+		String sb="ТокарьТочТив";
+		String destString = sb.toString().replace('т', 'о');
+		String destString1="" ;
+		destString1= destString.toString().replace('Т','О');
+		System.out.println(destString1);
+		    }
+		     
+		} 
